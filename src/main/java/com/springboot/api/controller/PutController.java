fad5f2794548d6d218977302c52c657dfb2172de
Hyeon <wkdgustn002@naver.com>
2022-07-11 20:01:00 +0900

    test

---
+++ src/main/java/com/springboot/api/controller/PutController.java
@@ -0,0 +1,47 @@
+package com.springboot.api.controller;
+
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.springboot.api.dto.MemberDto;
+
+@RestController
+@RequestMapping("/api/v1/put-api")
+public class PutController {
+    
+    @RequestMapping(value = "/main", method = RequestMethod.PUT)
+    public String putExample(){
+        return "Hellow";
+    }
+
+    // http://localhost:8080/api/v1/put-api/member
+    @PutMapping(value = "/member")
+    public String postMember(@RequestBody Map<String, Object> putData) {
+        StringBuilder sb = new StringBuilder();
+
+        putData.entrySet().forEach(map -> {
+            sb.append(map.getKey() + " : " + map.getValue() + "\n");
+        });
+
+        return sb.toString();
+    }
+
+    // http://localhost:8080/api/put-api/member1
+    @PutMapping(value = "/member1")
+    public String postMemberDto1(@RequestBody MemberDto memberDto) {
+        return memberDto.toString();
+    }
+
+    // http://localhost:8080/api/put-api/member2
+    @PutMapping(value = "/member2")
+    public MemberDto postMemberDto2(@RequestBody MemberDto memberDto) {
+        return memberDto;
+    }
+
+    
+}
